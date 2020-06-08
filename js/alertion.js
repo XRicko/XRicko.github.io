8be20ae79256a0f82ee2bf61f16c27ea6453cfad
XRicko <idims52@gmail.com>
2020-06-08 03:07:24 +0300

    create

---
+++ js/alertion.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    const youtubeBtn = document.querySelector('.fa-youtube');
+    youtubeBtn.addEventListener('click', () => {
+      alert('You`ve been rickrolled!');
+    });
+
+}());
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYWxlcnRpb24uanMiLCJzb3VyY2VzIjpbInNyYy9qcy9hbGVydGlvbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCB5b3V0dWJlQnRuID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLmZhLXlvdXR1YmUnKTtcclxuXHJcbnlvdXR1YmVCdG4uYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCAoKSA9PiB7XHJcbiAgICBhbGVydCgnWW91YHZlIGJlZW4gcmlja3JvbGxlZCEnKTtcclxufSk7Il0sIm5hbWVzIjpbInlvdXR1YmVCdG4iLCJkb2N1bWVudCIsInF1ZXJ5U2VsZWN0b3IiLCJhZGRFdmVudExpc3RlbmVyIiwiYWxlcnQiXSwibWFwcGluZ3MiOiI7OztJQUFBLE1BQU1BLFVBQVUsR0FBR0MsUUFBUSxDQUFDQyxhQUFULENBQXVCLGFBQXZCLENBQW5CO0lBRUFGLFVBQVUsQ0FBQ0csZ0JBQVgsQ0FBNEIsT0FBNUIsRUFBcUMsTUFBTTtJQUN2Q0MsRUFBQUEsS0FBSyxDQUFDLHlCQUFELENBQUw7SUFDSCxDQUZEOzs7OyJ9
